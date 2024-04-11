6470071b348ccd7f2d371779513bd14d1bd839ef
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-04-11 10:48:32 +0100

    Updated for sfi-23 staement new variables

--- app/publishing/publish-by-email.js
+++ app/publishing/publish-by-email.js
@@ -1,12 +1,14 @@
+const moment = require('moment')
 const config = require('../config')
 const { NotifyClient } = require('notifications-node-client')
 
 const publishByEmail = async (email, file, personalisation) => {
   const notifyClient = new NotifyClient(config.notifyApiKey)
+  const latestDownloadDate = moment(new Date()).add(config.retentionPeriodInWeeks, 'weeks').format('LL')
   return notifyClient.sendEmail(config.notifyEmailTemplateKey, email, {
     personalisation: {
       link_to_file: notifyClient.prepareUpload(file, { confirmEmailBeforeDownload: true, retentionPeriod: `${config.retentionPeriodInWeeks} weeks` }),
-      ...personalisation
+      ...personalisation, latestDownloadDate
     }
   })
 }
--- app/publishing/publish-statement.js
+++ app/publishing/publish-statement.js
@@ -23,7 +23,7 @@ const publishStatement = async (request) => {
 
   try {
     validateEmail(request.email)
-    const personalisation = getPersonalisation(request.scheme.name, request.scheme.shortName, request.scheme.year, request.scheme.frequency, request.businessName)
+    const personalisation = getPersonalisation(request.scheme.name, request.scheme.shortName, request.scheme.year, request.scheme.frequency, request.businessName, request.paymentPeriod)
     response = await publish(request.email, request.filename, personalisation)
     console.log(`Statement published: ${request.filename}`)
   } catch (err) {
