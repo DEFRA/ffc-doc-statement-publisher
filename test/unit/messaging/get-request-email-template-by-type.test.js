731144fddec64239b3c709aeeac5e48c54fca93a
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-04-22 14:09:06 +0100

    Modified test description IRO PR FB

--- test/unit/messaging/get-request-email-template-by-type.test.js
+++ test/unit/messaging/get-request-email-template-by-type.test.js
@@ -6,7 +6,7 @@ const mockDocumentTypes = require('../../mocks/objects/document-types')
 describe('get request email-template by type', () => {
   beforeEach(() => {
   })
-  test('should template when document type exists', () => {
+  test('should return template when document type exists', () => {
     const requestType = mockDocumentTypes[0].type
     const result = getRequestEmailTemplateByType(requestType, mockDocumentTypes)
     expect(result).toBe(mockDocumentTypes[0].template)
