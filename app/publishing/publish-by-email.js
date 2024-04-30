764b9128a148f96e2c494a4642a749796b0095c1
Abidemi Adio <abidemi.adio@defra.gov.uk>
2024-04-30 18:36:19 +0100

    Troubleshoot error 400 v2

--- app/publishing/publish-by-email.js
+++ app/publishing/publish-by-email.js
@@ -5,10 +5,14 @@ const { NotifyClient } = require('notifications-node-client')
 const publishByEmail = async (template, email, file, personalisation) => {
   moment.locale('en-gb')
   const notifyClient = new NotifyClient(config.notifyApiKey)
+  console.log(`Test-Env-Stage3-Publishing ${notifyClient} to ${email}`)
   const latestDownloadDate = moment(new Date()).add(config.retentionPeriodInWeeks, 'weeks').format('LL')
+  console.log(`Test-Env-Stage4-Publishing ${latestDownloadDate} to ${email}`)
+  const notifyAttachment = await notifyClient.prepareUpload(file, { confirmEmailBeforeDownload: true, retentionPeriod: `${config.retentionPeriodInWeeks} weeks` })
+  console.log(`Test-Env-Stage5-Publishing ${template} to ${email}`)
   return notifyClient.sendEmail(template, email, {
     personalisation: {
-      link_to_file: notifyClient.prepareUpload(file, { confirmEmailBeforeDownload: true, retentionPeriod: `${config.retentionPeriodInWeeks} weeks` }),
+      link_to_file: notifyAttachment,
       ...personalisation,
       latestDownloadDate
     }
--- app/publishing/publish.js
+++ app/publishing/publish.js
@@ -3,7 +3,9 @@ const { getFile } = require('../storage')
 const publishByEmail = require('./publish-by-email')
 
 const publish = async (template, email, filename, personalisation) => {
+  console.log(`Test-Env-Stage1-Publishing ${filename} to ${email}`)
   const file = await retry(() => getFile(filename))
+  console.log(`Test-Env-Stage2-Publishing ${filename} to ${email}`)
   return publishByEmail(template, email, file, personalisation)
 }
 
