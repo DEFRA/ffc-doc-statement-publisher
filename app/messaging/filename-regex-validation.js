d83e754b09e5efd55364286a3dc9bcd4aebf76ca
JungaBunga <amir.ishmuhametov@gmail.com>
2024-06-20 11:55:09 +0100

    one more refactor in hope it will pass sonarcloud

--- app/messaging/filename-regex-validation.js
+++ app/messaging/filename-regex-validation.js
@@ -1,37 +1,55 @@
-const teamName = /[A-Z]{3,6}_/
-const documentPrefix = /[A-Z](?:[A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*_/
-const schemeShortName = /[A-Z]{3,6}_/
-const schemeYear = /\d{4}_/
-const frn = /\d{10}_/
-const timestampRegex = /\d{16}/
-const extension = /\.pdf$/
-
-function matchPattern (str) {
-  let matched = str.match(teamName)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  matched = str.match(documentPrefix)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  matched = str.match(schemeShortName)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  matched = str.match(schemeYear)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  matched = str.match(frn)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  matched = str.match(timestampRegex)
-  if (!matched) return false
-  str = str.slice(matched[0].length)
-
-  return extension.test(str)
+const filenameRegex = {
+  teamName: /^[A-Z]{3,6}_/,
+  documentPrefix: /^[A-Z](?:[A-Z0-9]*[a-z][a-z0-9]*[A-Z]|[a-z0-9]*[A-Z][A-Z0-9]*[a-z])[A-Za-z0-9]*_/,
+  schemeShortName: /^[A-Z]{3,6}_/,
+  schemeYear: /^\d{4}_/,
+  frn: /^\d{10}_/,
+  timestampRegex: /\d{16}/,
+  extension: /\.pdf$/
 }
 
-module.exports = matchPattern
+const validateFilename = (filename) => {
+  const { teamName, documentPrefix, schemeShortName, schemeYear, frn, timestampRegex, extension } = filenameRegex
+
+  try {
+    if (!teamName.test(filename)) {
+      throw new Error('invalid team name')
+    }
+    filename = filename.replace(teamName, '')
+
+    if (!documentPrefix.test(filename)) {
+      throw new Error('invalid document prefix')
+    }
+    filename = filename.replace(documentPrefix, '')
+
+    if (!schemeShortName.test(filename)) {
+      throw new Error('invalid scheme short name')
+    }
+    filename = filename.replace(schemeShortName, '')
+
+    if (!schemeYear.test(filename)) {
+      throw new Error('invalid scheme year')
+    }
+    filename = filename.replace(schemeYear, '')
+
+    if (!frn.test(filename)) {
+      throw new Error('invalid frn number')
+    }
+    filename = filename.replace(frn, '')
+
+    if (!timestampRegex.test(filename)) {
+      throw new Error('invalid timestamp')
+    }
+    filename = filename.replace(timestampRegex, '')
+
+    if (!extension.test(filename)) {
+      throw new Error('invalid extension')
+    }
+  } catch (error) {
+    return false
+  }
+
+  return true
+}
+
+module.exports = validateFilename
