8de2913274cda49e453da899b897f1679055e127
JungaBunga <amir.ishmuhametov@gmail.com>
2024-06-20 12:05:29 +0100

    added more test coverage

--- test/unit/messaging/filename-regex-validation.test.js
+++ test/unit/messaging/filename-regex-validation.test.js
@@ -1,14 +1,43 @@
-const matchPattern = require('../../../app/messaging/filename-regex-validation')
-const mockMessagingSchema = require('../../mocks/messaging-schema')
+const validateFilename = require('../../../app/messaging/filename-regex-validation')
 
-describe('matchPattern', () => {
+describe('validateFilename', () => {
   test('validates a string that matches the pattern', () => {
-    const str = mockMessagingSchema.filename
-    expect(matchPattern(str)).toBe(true)
+    const validFilename = 'FFC_PaymentStatement_SFI_2022_1234567890_2022080515301012.pdf'
+    expect(validateFilename(validFilename)).toBe(true)
   })
 
-  test('returns false for a string that does not match the pattern', () => {
-    const wrongFilename = mockMessagingSchema.filename + 'wrong'
-    expect(matchPattern(wrongFilename)).toBe(false)
+  test('returns false for a string with invalid team name', () => {
+    const invalidTeamName = 'FF_PaymentStatement_SFI_2022_1234567890_2022080515301012.pdf'
+    expect(validateFilename(invalidTeamName)).toBe(false)
+  })
+
+  test('returns false for a string with invalid document prefix', () => {
+    const invalidDocumentPrefix = 'FFC_123PaymentStatement_SFI_2022_1234567890_2022080515301012.pdf'
+    expect(validateFilename(invalidDocumentPrefix)).toBe(false)
+  })
+
+  test('returns false for a string with invalid scheme short name', () => {
+    const invalidSchemeShortName = 'FFC_PaymentStatement_S_2022_1234567890_2022080515301012.pdf'
+    expect(validateFilename(invalidSchemeShortName)).toBe(false)
+  })
+
+  test('returns false for a string with invalid scheme year', () => {
+    const invalidSchemeYear = 'FFC_PaymentStatement_SFI_22_1234567890_2022080515301012.pdf'
+    expect(validateFilename(invalidSchemeYear)).toBe(false)
+  })
+
+  test('returns false for a string with invalid FRN number', () => {
+    const invalidFrn = 'FFC_PaymentStatement_SFI_2022_123456789_2022080515301012.pdf'
+    expect(validateFilename(invalidFrn)).toBe(false)
+  })
+
+  test('returns false for a string with invalid timestamp', () => {
+    const invalidTimestamp = 'FFC_PaymentStatement_SFI_2022_1234567890_202208051530101.pdf'
+    expect(validateFilename(invalidTimestamp)).toBe(false)
+  })
+
+  test('returns false for a string with invalid extension', () => {
+    const invalidExtension = 'FFC_PaymentStatement_SFI_2022_1234567890_2022080515301012.docx'
+    expect(validateFilename(invalidExtension)).toBe(false)
   })
 })
