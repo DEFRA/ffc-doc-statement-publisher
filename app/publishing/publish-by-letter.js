82fc51b569be76352c6dc54cd10564cf39874b5c
John Barnard <john.barnard.external@eviden.com>
2024-11-13 14:10:54 +0000

    DOC-1433: use retry function for publish by letter

--- app/publishing/publish-by-letter.js
+++ app/publishing/publish-by-letter.js
@@ -1,10 +1,26 @@
 const config = require('../config')
 const { NotifyClient } = require('notifications-node-client')
 const { SECOND } = require('../constants/postage')
+const { retry } = require('../retry')
+
+const setupLetterRequest = (reference, pdfFile, postage, notifyClient) => {
+  return () => {
+    return notifyClient.sendPrecompiledLetter(reference, pdfFile, postage)
+  }
+}
 
 const publishByLetter = async (reference, pdfFile, postage = SECOND) => {
   const notifyClient = new NotifyClient(config.notifyApiKey)
-  return notifyClient.sendPrecompiledLetter(reference, pdfFile, postage)
+  const letterRequest = setupLetterRequest(reference, pdfFile, postage, notifyClient)
+  return retry(letterRequest, 3, 100, true)
+    .then(result => {
+      console.log(result)
+      return result
+    })
+    .catch((err) => {
+      console.log(err)
+      throw err
+    })
 }
 
 module.exports = publishByLetter
