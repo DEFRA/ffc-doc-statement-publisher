16e8c298bde143c5a36341f77c898a7c80f0f5ad
John Barnard <john.barnard.external@eviden.com>
2024-11-13 12:12:21 +0000

    DOC-1433: removed thunkify from retry logic

--- app/publishing/publish-by-email.js
+++ app/publishing/publish-by-email.js
@@ -1,55 +1,24 @@
 const moment = require('moment')
 const config = require('../config')
 const { NotifyClient } = require('notifications-node-client')
-const { retry, thunkify } = require('../retry')
-const promisify = require('../promisify')
+const { retry } = require('../retry')
 
-function setupNotifyClient () {
-  const notifyClient = new NotifyClient(config.notifyApiKey)
-  notifyClient.prepareUpload = promisify(notifyClient.prepareUpload)
-  notifyClient.sendEmail = promisify(notifyClient.sendEmail)
-  return notifyClient
-}
-
-/**
- * Creates a thunk for the retry mechanism
- * A thunk is basically an instance of a function call along with parameter values that can be evaluated at any time
- * For example the following function
- * function toBeThunked(a,b) return a+b
- * Can be turned into a thunk like this
- * function thunkify(fn){
- *   const args = [].slice.call(arguments, 1)
- *   return function(cb) {
- *     args.push(cb)
- *     return fn.apply(null,args)
- *   }
- * }
- * const thunk = thunkify(toBeThunked, 2, 2)
- * console.log(thunk()) // prints 4
- * console.log(thunk()) // prints 4 again even though we did nothing different
- *
- *
- * @param {Object} template
- * @param {String} email
- * @param {String} file
- * @param {Object} personalisation
- * @param {NotifyClient} notifyClient
- * @returns Thunk
- */
-function setupEmailThunk (template, email, linkToFile, personalisation, notifyClient) {
+function setupEmailRequest (template, email, linkToFile, personalisation, notifyClient) {
   const latestDownloadDate = moment(new Date()).add(config.retentionPeriodInWeeks, 'weeks').format('LL')
-  return promisify(thunkify(notifyClient.sendEmail, template, email, {
-    personalisation: {
-      link_to_file: linkToFile,
-      ...personalisation,
-      latestDownloadDate
-    }
-  }))
+  return () => {
+    return notifyClient.sendEmail(template, email, {
+      personalisation: {
+        link_to_file: linkToFile,
+        ...personalisation,
+        latestDownloadDate
+      }
+    })
+  }
 }
 
 const publishByEmail = async (template, email, file, personalisation) => {
   moment.locale('en-gb')
-  const notifyClient = setupNotifyClient()
+  const notifyClient = new NotifyClient(config.notifyApiKey)
   const linkToFile = await notifyClient.prepareUpload(
     file,
     {
@@ -57,8 +26,8 @@ const publishByEmail = async (template, email, file, personalisation) => {
       retentionPeriod: `${config.retentionPeriodInWeeks} weeks`
     }
   )
-  const thunk = setupEmailThunk(template, email, linkToFile, personalisation, notifyClient)
-  return retry(thunk, 3, 100, true)
+  const emailRequest = setupEmailRequest(template, email, linkToFile, personalisation, notifyClient)
+  return retry(emailRequest, 3, 100, true)
     .then(result => {
       console.log(result)
       return result
--- app/retry.js
+++ app/retry.js
@@ -1,21 +1,3 @@
-/**
- * Turns a regular function into a thunk for evaluation at a later time
- * For more information on thunks see https://en.wikipedia.org/wiki/Thunk#:~:text=In%20computer%20programming%2C%20a%20thunk,end%20of%20the%20other%20subroutine.
- *
- * @param {Function} fn any function (sync or async) you wish to turn into a thunk
- * @param {ParamArray} an array of parameters to be used for the function
- * see here for more info on rest parameters https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/rest_parameters
- * @returns
- */
-function thunkify (fn, ...rest) {
-  return function (cb) {
-    if (cb) {
-      rest.push(cb)
-    }
-    return fn(...rest)
-  }
-}
-
 const retry = async (fn, retriesLeft = 10, interval = 5000, exponential = false) => {
   try {
     return await fn()
@@ -30,6 +12,5 @@ const retry = async (fn, retriesLeft = 10, interval = 5000, exponential = false)
 }
 
 module.exports = {
-  retry,
-  thunkify
+  retry
 }
