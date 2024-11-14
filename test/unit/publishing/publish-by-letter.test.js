66960abecc1b992296081659d3a9ecef471b1168
John Barnard <john.barnard.external@eviden.com>
2024-11-14 10:19:43 +0000

    DOC-1433: updated publish by letter tests to test retry

--- test/unit/publishing/publish-by-letter.test.js
+++ test/unit/publishing/publish-by-letter.test.js
@@ -1,15 +1,28 @@
 const config = require('../../../app/config')
-
 const { mockNotifyClient } = require('../../mocks/modules/notifications-node-client')
-
 const publishByLetter = require('../../../app/publishing/publish-by-letter')
+const { retry } = require('../../../app/retry')
 
 const EMAIL = require('../../mocks/components/email')
 const FILE = require('../../mocks/components/filename')
 const FILE_BUFFER = require('../../mocks/components/file_buffer')
 const POSTAGE = require('../../mocks/components/postage')
 
-describe('Publish by email', () => {
+jest.mock('../../../app/retry', () => {
+  const originalModule = jest.requireActual('../../../app/retry')
+  return {
+    ...originalModule,
+    retry: jest.fn((...args) => originalModule.retry(...args))
+  }
+})
+
+describe('Publish by letter', () => {
+  let retrySpy
+
+  beforeEach(() => {
+    retrySpy = require('../../../app/retry').retry
+  })
+
   afterEach(() => {
     jest.clearAllMocks()
   })
@@ -41,12 +54,29 @@ describe('Publish by email', () => {
 
   test('should call mockNotifyClient.sendPrecompiledLetter with FILE, FILE_BUFFER and default POSTAGE value "second"', async () => {
     await publishByLetter(FILE, FILE_BUFFER)
-
     expect(mockNotifyClient().sendPrecompiledLetter).toHaveBeenCalledWith(FILE, FILE_BUFFER, POSTAGE)
   })
 
   test('should return mockNotifyClient.sendPrecompiledLetter', async () => {
     const result = await publishByLetter(FILE, FILE_BUFFER)
     expect(result).toBe(await mockNotifyClient().sendPrecompiledLetter())
+  })
+
+  test('should retry the letter request on failure', async () => {
+    const error = new Error('Test error')
+    mockNotifyClient().sendPrecompiledLetter.mockRejectedValueOnce(error).mockResolvedValueOnce('Letter sent')
+
+    await expect(publishByLetter(FILE, FILE_BUFFER)).resolves.toBe('Letter sent')
+    expect(retrySpy).toHaveBeenCalled()
+    expect(retrySpy).toHaveBeenCalledWith(expect.any(Function), 3, 100, true)
+  })
+
+  test('should fail after retrying the letter request three times', async () => {
+    const error = new Error('Test error')
+    mockNotifyClient().sendPrecompiledLetter.mockRejectedValue(error)
+
+    await expect(publishByLetter(FILE, FILE_BUFFER)).rejects.toThrow('Test error')
+    expect(retrySpy).toHaveBeenCalled()
+    expect(retrySpy).toHaveBeenCalledWith(expect.any(Function), 3, 100, true)
   })
 })
