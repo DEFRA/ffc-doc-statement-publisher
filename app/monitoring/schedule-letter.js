ede1e08928814bb6954c9a92cf880c1351dba08c
John Barnard <john.barnard.external@eviden.com>
2024-11-25 13:10:07 +0000

    DOC-1003: simplified helper

--- app/monitoring/schedule-letter.js
+++ app/monitoring/schedule-letter.js
@@ -1,7 +1,7 @@
 const { LETTER } = require('../constants/methods')
 const db = require('../data')
 const publish = require('../publishing/publish')
-const isDpSchema = require('../publishing/is-dp-scheme')
+const isDpScheme = require('../publishing/is-dp-scheme')
 
 const scheduleLetter = async (delivery) => {
   const transaction = await db.sequelize.transaction()
@@ -9,7 +9,7 @@ const scheduleLetter = async (delivery) => {
     const timestamp = new Date()
     const statement = await db.statement.findOne({ where: { statementId: delivery.statementId }, transaction })
 
-    if (isDpSchema(statement?.schemeShortName)) {
+    if (isDpScheme(statement?.schemeShortName)) {
       const response = await publish(statement.emailTemplate, statement.email, statement?.filename, null, LETTER)
       await db.delivery.create({ statementId: delivery.statementId, method: delivery.method, reference: response.data.id, requested: timestamp }, { transaction })
       await db.delivery.update({ completed: timestamp }, { where: { deliveryId: delivery.deliveryId }, transaction })
