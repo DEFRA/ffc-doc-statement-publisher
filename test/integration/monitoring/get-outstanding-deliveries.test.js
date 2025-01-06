676bd1d8864ba2f49932c52371b45cd3333a5be9
John Barnard <john.barnard.external@eviden.com>
2025-01-06 15:47:43 +0000

    Removed mocks from integration tests

--- test/integration/monitoring/get-outstanding-deliveries.test.js
+++ test/integration/monitoring/get-outstanding-deliveries.test.js
@@ -1,25 +1,5 @@
-jest.mock('../../../app/data', () => ({
-  sequelize: {
-    truncate: jest.fn(),
-    close: jest.fn()
-  },
-  statement: {
-    bulkCreate: jest.fn()
-  },
-  delivery: {
-    bulkCreate: jest.fn(),
-    update: jest.fn(),
-    findAll: jest.fn()
-  },
-  Sequelize: {
-    Op: {
-      ne: Symbol('ne')
-    }
-  }
-}))
-
-const getOutstandingDeliveries = require('../../../app/monitoring/get-outstanding-deliveries')
 const db = require('../../../app/data')
+const getOutstandingDeliveries = require('../../../app/monitoring/get-outstanding-deliveries')
 const { mockDelivery1, mockDelivery2 } = require('../../mocks/delivery')
 const { mockStatement1, mockStatement2 } = require('../../mocks/statement')
 
@@ -39,20 +19,16 @@ describe('get outstanding deliveries', () => {
   })
 
   test('returns correct number of outstanding deliveries', async () => {
-    require('../../../app/data').delivery.findAll.mockResolvedValue([mockDelivery1])
     const result = await getOutstandingDeliveries()
     expect(result.length).toBe(1)
   })
 
   test('returns correct outstanding delivery', async () => {
-    require('../../../app/data').delivery.findAll.mockResolvedValue([mockDelivery1])
     const result = await getOutstandingDeliveries()
     expect(result[0].deliveryId).toBe(mockDelivery1.deliveryId)
   })
 
   test('does not return delivery without reference', async () => {
-    require('../../../app/data').delivery.findAll.mockResolvedValue([])
-
     await db.delivery.update({ reference: null }, { where: { deliveryId: mockDelivery1.deliveryId } })
     const result = await getOutstandingDeliveries()
     expect(result.length).toBe(0)
