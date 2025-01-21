f4b0e41ff4eb885c58773fe83ec9941cf60fd13a
John Barnard <john.barnard.external@eviden.com>
2025-01-21 15:19:46 +0000

    Use constants

--- app/reporting/get-todays-report.js
+++ app/reporting/get-todays-report.js
@@ -1,9 +1,13 @@
 const db = require('../data')
+const hour = 23
+const minute = 59
+const second = 59
+const millisecond = 999
 
 const getTodaysReport = async (schemeName) => {
   const today = new Date()
   const startOfDay = new Date(today.setHours(0, 0, 0, 0))
-  const endOfDay = new Date(today.setHours(23, 59, 59, 999))
+  const endOfDay = new Date(today.setHours(hour, minute, second, millisecond))  
 
   return db.report.findAll({
     where: {
