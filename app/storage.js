b2cdce70f4edd0965f503176450fe89e095c25a6
Oliver Lewington <oliver.lewington@eviden.com>
2025-02-18 11:49:10 +0000

    Adding checks for storage.js

--- app/storage.js
+++ app/storage.js
@@ -20,7 +20,9 @@ const initialiseContainers = async () => {
     console.log('Making sure blob containers exist')
     await container.createIfNotExists()
   }
+
   await initialiseFolders()
+
   containersInitialised = true
 }
 
@@ -30,19 +32,19 @@ const initialiseFolders = async () => {
   await client.upload(placeHolderText, placeHolderText.length)
 }
 
-const getBlob = async (filename) => {
+const getOutboundBlobClient = async (filename) => {
   containersInitialised ?? await initialiseContainers()
   return container.getBlockBlobClient(`${config.folder}/${filename}`)
 }
 
 const getFile = async (filename) => {
-  const blob = await getBlob(filename)
+  const blob = await getOutboundBlobClient(filename)
   return blob.downloadToBuffer()
 }
 
 module.exports = {
   initialiseContainers,
   blobServiceClient,
-  getBlob,
+  getOutboundBlobClient,
   getFile
 }
