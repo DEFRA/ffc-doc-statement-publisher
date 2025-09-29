513f90b1b09a98daf7ce3df0294d33149af01642
John Barnard <john.barnard.external@eviden.com>
2025-09-29 13:43:54 +0100

    use ffc-database
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- app/data/index.js
+++ app/data/index.js
@@ -1,30 +1,10 @@
-const fs = require('fs')
 const path = require('path')
-const { Sequelize, DataTypes, Op } = require('sequelize')
 const config = require('../config')
 const dbConfig = config.dbConfig[config.env]
 const modelPath = path.join(__dirname, 'models')
-const db = {}
+const { Database } = require('ffc-database')
 
-const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig)
-
-fs.readdirSync(modelPath)
-  .filter(file => {
-    return (!file.startsWith('.')) && (file !== 'index.js') && (file.endsWith('.js'))
-  })
-  .forEach(file => {
-    const model = require(path.join(modelPath, file))(sequelize, DataTypes)
-    db[model.name] = model
-  })
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db)
-  }
-})
-
-db.sequelize = sequelize
-db.Sequelize = Sequelize
-db.Op = Op
+const database = new Database({ ...dbConfig, modelPath })
+const db = database.connect()
 
 module.exports = db
