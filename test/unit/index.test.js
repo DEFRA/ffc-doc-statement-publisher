9b6d9a2b8376cd0ded75c507be530cfb2e0a60d2
John Barnard <john.barnard.external@eviden.com>
2025-10-02 13:17:22 +0100

    Added alerting setup tests to index test file
    Signed-off-by: John Barnard <john.barnard.external@eviden.com>

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,11 +1,24 @@
 jest.mock('../../app/messaging')
 const mockMessaging = require('../../app/messaging')
+
 jest.mock('../../app/monitoring')
 const mockMonitoring = require('../../app/monitoring')
+
 jest.mock('../../app/storage')
 const mockStorage = require('../../app/storage')
+
 jest.mock('../../app/reporting')
 const mockReporting = require('../../app/reporting')
+
+jest.mock('ffc-alerting-utils', () => ({
+  init: jest.fn()
+}))
+const mockAlerting = require('ffc-alerting-utils')
+
+jest.mock('../../app/insights', () => ({
+  setup: jest.fn()
+}))
+const mockInsights = require('../../app/insights')
 
 describe('app', () => {
   beforeEach(() => {
@@ -26,5 +39,13 @@ describe('app', () => {
 
   test('initialises containers', async () => {
     expect(mockStorage.initialiseContainers).toHaveBeenCalled()
+  })
+
+  test('initialises alerting if available', async () => {
+    expect(mockAlerting.init).toHaveBeenCalled()
+  })
+
+  test('sets up insights', async () => {
+    expect(mockInsights.setup).toHaveBeenCalled()
   })
 })
