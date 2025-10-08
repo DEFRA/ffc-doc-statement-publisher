91e55cea8a5078268b3254ed722d5b6bfe5da990
Sam Plackett <samuel.plackett@eviden.com>
2025-10-08 11:16:43 +0100

    add template

--- test/mocks/objects/document-types.js
+++ test/mocks/objects/document-types.js
@@ -2,7 +2,8 @@ module.exports = [
   {
     id: 'sfi-23-quarterly-statement',
     type: 'uk.gov.doc.sfi-23-quarterly-statement.publish',
-    name: 'SFI 23 quarterly statement'
+    name: 'SFI 23 quarterly statement',
+    template: 'af20f680-43ef-4702-a5e0-832fe967f3a6'
   },
   {
     id: 'delinked-statement',
