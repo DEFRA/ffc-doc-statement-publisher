f0e4cffa0ab74221cf676d70a47b220c27b7dd2a
Leigh Godson <leigh.godson@atos.net>
2026-02-03 12:59:32 +0000

    sonar fix

--- test/unit/server/routes/statement.test.js
+++ test/unit/server/routes/statement.test.js
@@ -7,7 +7,6 @@ describe('statements route', () => {
   })
 
   test('returns payload with parsed values', async () => {
-    // Mock the statement model with sample data
     jest.doMock('../../../../app/data/models/statement', () => ({
       filename: 'file.csv',
       schemeId: '1',
@@ -30,8 +29,30 @@ describe('statements route', () => {
     })
   })
 
+  test('returns payload with null values when properties are missing', async () => {
+    jest.doMock('../../../../app/data/models/statement', () => ({
+      filename: null,
+      schemeId: null,
+      marketingYear: null,
+      frn: null,
+      timestamp: '2020-01-01T00:00:00Z'
+    }))
+
+    const routes = require('../../../../app/server/routes/statements')
+    const handler = routes[0].handler
+
+    const result = await handler()
+
+    expect(result).toEqual({
+      filename: null,
+      schemeId: null,
+      marketingYear: null,
+      frn: null,
+      timestamp: '2020-01-01T00:00:00Z'
+    })
+  })
+
   test('returns h.response on error', async () => {
-    // Mock the statement model to throw an error
     const throwing = new Proxy({}, { get: () => { throw new Error('boom') } })
     jest.doMock('../../../../app/data/models/statement', () => throwing)
 
