e30d7468ac32903ebb30a98833d4cdcd86b8744e
Leigh Godson <leigh.godson@atos.net>
2026-02-05 16:26:33 +0000

    fix flakey millisecond test failures in metrics and reporting

--- test/integration/reporting/complete-report.test.js
+++ test/integration/reporting/complete-report.test.js
@@ -3,12 +3,23 @@ const completeReport = require('../../../app/reporting/complete-report')
 const { mockReport1, mockReport2 } = require('../../mocks/report')
 
 describe('completeReport', () => {
+  let transaction
+
   beforeEach(async () => {
     jest.clearAllMocks()
     jest.useFakeTimers().setSystemTime(new Date(2022, 7, 5, 15, 30, 10, 120))
 
     await db.sequelize.truncate({ cascade: true })
     await db.report.bulkCreate([mockReport1, mockReport2])
+
+    transaction = await db.sequelize.transaction()
+  })
+
+  afterEach(async () => {
+    if (transaction && !transaction.finished) {
+      await transaction.rollback()
+    }
+    jest.useRealTimers()
   })
 
   afterAll(async () => {
@@ -16,10 +27,12 @@ describe('completeReport', () => {
   })
 
   test('marks the report as sent', async () => {
-    const transaction = await db.sequelize.transaction()
     const lastDeliveryId = 1234
     await completeReport(mockReport1.reportId, lastDeliveryId, transaction)
+    await transaction.commit()
+
     const updatedReport = await db.report.findByPk(mockReport1.reportId)
+    expect(updatedReport).not.toBeNull()
     expect(updatedReport.sent).toEqual(new Date(2022, 7, 5, 15, 30, 10, 120))
   })
 })
